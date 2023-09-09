b19145d813979920bc0fea5272483cd99d9bfed7
John Watson <john.watson1@defra.gov.uk>
2023-09-09 22:24:40 +0100

    Add tests

--- app/transfer/get-data-filename.js
+++ app/transfer/get-data-filename.js
@@ -4,11 +4,11 @@ const getDataFilename = (controlFilename, direction) => {
   if (controlFilename.startsWith('CTL_')) {
     return controlFilename.replace('CTL_', '')
   }
-  if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('GENESIS')) {
-    return direction === INBOUND ? controlFilename.replace('.ctl', '.gne') : controlFilename.replace('.ctl', '.gni')
-  }
   if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('FCAP')) {
     return controlFilename.replace('.ctl', '.dat')
+  }
+  if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('GENESIS')) {
+    return direction === INBOUND ? controlFilename.replace('.ctl', '.gne') : controlFilename.replace('.ctl', '.gni')
   }
 }
 
---
+++ test/unit/transfer/get-data-filename.test.js
@@ -0,0 +1,25 @@
+const { INBOUND, OUTBOUND } = require('../../../app/constants/directions')
+
+const { getDataFilename } = require('../../../app/transfer/get-data-filename')
+
+describe('get data filename', () => {
+  test('should return filename without prefix if control file prefix', () => {
+    const result = getDataFilename('CTL_test.txt')
+    expect(result).toBe('test.txt')
+  })
+
+  test('should return filename with .dat extension if GLOS control file', () => {
+    const result = getDataFilename('FCAP_test.ctl')
+    expect(result).toBe('FCAP_test.dat')
+  })
+
+  test('should return filename with .gne extension if inbound GENESIS control file', () => {
+    const result = getDataFilename('GENESIS_test.ctl', INBOUND)
+    expect(result).toBe('GENESIS_test.gne')
+  })
+
+  test('should return filename with .gni extension if outbound GENESIS control file', () => {
+    const result = getDataFilename('GENESIS_test.ctl', OUTBOUND)
+    expect(result).toBe('GENESIS_test.gni')
+  })
+})
