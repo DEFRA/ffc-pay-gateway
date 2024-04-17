f4c513daa62498dfc33734a188b3612d3fae45a4
John Watson <john.watson1@defra.gov.uk>
2024-04-17 16:22:31 +0100

    Simplify

--- app/transfer/get-data-filename.js
+++ app/transfer/get-data-filename.js
@@ -7,9 +7,6 @@ const getDataFilename = (controlFilename, direction) => {
   if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('FCAP')) {
     return controlFilename.replace('.ctl', '.dat')
   }
-  if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('GENESIS') && direction === INBOUND) {
-    return controlFilename.replace('.ctl', '')
-  }
   if (controlFilename.endsWith('.ctl') && controlFilename.startsWith('GENESIS')) {
     return controlFilename.replace('.ctl', '')
   }
