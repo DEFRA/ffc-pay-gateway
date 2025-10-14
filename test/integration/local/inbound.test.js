11853f9d012cc829f7d45afb276cabc12266008b
John Barnard <john.barnard.external@eviden.com>
2025-10-14 15:55:06 +0100

    Fixed inwqbound IMPS test by using fake timers to ensure we are in the default IMPS window
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- test/integration/local/inbound.test.js
+++ test/integration/local/inbound.test.js
@@ -288,6 +288,11 @@ describe('process inbound files', () => {
   })
 
   test('should transfer IMPS data files to batch inbound location with pending filename', async () => {
+    // Set system time to Monday 10:00 (inside default pollWindow and pollDays)
+    jest.useFakeTimers().setSystemTime(new Date('2025-10-13T10:00:00Z'))
+    schemeConfig.imps.pollWindow = { start: '08:00', end: '18:00' }
+    schemeConfig.imps.pollDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+
     await uploadFile(IMPS_DATA_FILENAME)
     await uploadFile(IMPS_CONTROL_FILENAME)
 
@@ -296,6 +301,8 @@ describe('process inbound files', () => {
     const fileList = await getBlobs()
     expect(fileList.find(x => x === IMPS_DATA_FILENAME_PENDING)).toBeDefined()
     expect(fileList.find(x => x === IMPS_CONTROL_FILENAME_PENDING)).toBeDefined()
+
+    jest.useRealTimers()
   })
 
   test('should transfer DPS data files to batch inbound location with pending filename', async () => {
