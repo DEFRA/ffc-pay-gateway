e6e1cf1a05825b0e571cedad6474e5d6cf0a3816
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-08-06 16:48:50 +0100

    add algorithms for key reading for MG to support Azure migration (#75)

--- app/config/sftp.js
+++ app/config/sftp.js
@@ -55,7 +55,7 @@ if (result.value.debug) {
 
 if (process.env.NODE_ENV === 'production') {
   result.value.trader.algorithms = { kex: ['diffie-hellman-group14-sha256'] }
-  result.value.managedGateway.algorithms = { kex: ['diffie-hellman-group1-sha1'] }
+  result.value.managedGateway.algorithms = { kex: ['diffie-hellman-group14-sha256', 'diffie-hellman-group1-sha1'] }
 }
 
 module.exports = result.value
